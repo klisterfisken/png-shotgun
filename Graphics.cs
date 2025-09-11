f4df2c523659a6e6ded44a8df5427333bf8caf2f
Hanna <206832485+klisterfisken@users.noreply.github.com>
2025-09-11 16:54:40 +0200

    Add file containing Header method

---
+++ Graphics.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Shotgun
+{
+    class Graphics
+    {
+        public static void Header()
+        {
+            Console.WriteLine(@$"  __  __  __   ___  ______  ___  __ __ __  __");
+            Console.WriteLine(@$" (( \ ||  ||  // \\ | || | // \\ || || ||\ ||");
+            Console.WriteLine(@$"  \\  ||==|| ((   ))  ||  (( ___ || || ||\\||");
+            Console.WriteLine(@$" \_)) ||  ||  \\_//   ||   \\_|| \\_// || \||");
+            Console.WriteLine("______________________________________________");
+        }
+    }
+}
