e7f63ee4e8b98e6d95a7ee29f1b946f70390ee82
Hanna <206832485+klisterfisken@users.noreply.github.com>
2025-09-11 16:18:23 +0200

    Add ASCII Art header

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Shotgun
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(@$"  __  __  __   ___   ______   ___  __ __ __  __");
+            Console.WriteLine(@$" (( \ ||  ||  // \\  | || |  // \\ || || ||\ ||");
+            Console.WriteLine(@$"  \\  ||==|| ((   ))   ||   (( ___ || || ||\\||");
+            Console.WriteLine(@$" \_)) ||  ||  \\_//    ||    \\_|| \\_// || \||");
+            Console.ReadKey();
+        }
+    }
+}
