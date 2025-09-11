60d811793931d2433b1b8d91010e798e831d95d1
Hanna <206832485+klisterfisken@users.noreply.github.com>
2025-09-11 17:44:41 +0200

    Collect player info

--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace Shotgun
     {
         static void Main()
         {
+            bool player1CPU;
             Graphics.Header();
+            Console.Write("\nDags att spela shotgun!" +
+                          "\nSka spelare ett styras av en dator? j/n? ");
+            if (Console.ReadLine() == "n") player1CPU = false;
+            Console.Write("\nFyll i namnet på spelare ett: ");
+            string player1Name = Console.ReadLine();
             Console.ReadKey();
         }
     }
