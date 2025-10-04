29a5b1f576294a35b373910409e8d9edabee7c48
Hanna <206832485+klisterfisken@users.noreply.github.com>
2025-10-04 18:59:31 +0200

    Move method for player name to separate class

--- Player.cs
+++ Player.cs
@@ -1,19 +1,21 @@
 using System;
 using System.Dynamic;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Shotgun
 {
     public class Player
     {
         public string PlayerName { get; set; }
-        public bool CPU { get; set; }
+        //public bool CPU { get; set; } - Välj om spelaren ska styras av en dator eller ej
         public int Bullets { get; set; }
 
         public Player(string playerName, bool cpu)
         {
             PlayerName = playerName;
-            CPU = cpu;
+            //CPU = cpu;
             Bullets = 0;
         }
+
     }
 }
