50e60faf803c0320762e08912e4ccd4883bfba8e
Hanna <206832485+klisterfisken@users.noreply.github.com>
2025-10-04 19:04:29 +0200

    Add separate class for cpu

---
+++ Cpu.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Shotgun
+{
+    class Cpu
+    {
+        public static string CpuName()
+        {
+            Random random = new Random();
+            string cpuName = "Donuthelloh";
+            return cpuName;
+        }
+    }
+}
