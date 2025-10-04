3e27e26c797b27731891875fd96359e8150ac3a1
Hanna <206832485+klisterfisken@users.noreply.github.com>
2025-10-04 19:00:20 +0200

    Move method for player name to separate class

--- Program.cs
+++ Program.cs
@@ -8,27 +8,10 @@ namespace Shotgun
         {
             Graphics.Header();
             Console.WriteLine("\nDags att spela shotgun!");
-            bool player1cpu = PlayerCPU(1);
-            string player1Name = PlayerName(1);
-            bool player2cpu = PlayerCPU(2);
-            string player2Name = PlayerName(2);
-            Console.WriteLine($"\nSpelare 1 heter {player1Name} och styrs av en {(player1cpu ? "dator" : "person")}.");
-            Console.WriteLine($"Spelare 2 heter {player2Name} och styrs av en {(player2cpu ? "dator" : "person")}.");
+            string playerName = Player.PlayerName();
+            string cpuName = Cpu.CpuName();
+            Console.WriteLine($"\nI ena hörnan bla bla bla");
             Console.ReadKey();
-        }
-        static bool PlayerCPU(int playerNumber)
-        {
-            bool playerCPU = true;
-            Console.Write($"\nSka spelare {playerNumber} styras av en dator? j/n? ");
-            string? answer = Console.ReadLine();
-            if (answer.ToUpper() == "N") playerCPU = false;
-            return playerCPU;
-        }
-        static string PlayerName(int playerNumber)
-        {
-            Console.Write($"Fyll i namnet på spelare {playerNumber}: ");
-            string? playerName = Console.ReadLine();
-            return playerName;
         }
     }
 }
