fc482fc4bbe20bf84bde0ce1d3b4bb5a2b093072
Hanna <206832485+klisterfisken@users.noreply.github.com>
2025-10-05 17:18:58 +0200

    Add while loop

--- Program.cs
+++ Program.cs
@@ -6,20 +6,28 @@ namespace Shotgun
     {
         static void Main()
         {
+            bool power = true;
             Console.Clear();
             Graphics.Header();
             Music.Play();
             Console.WriteLine("\nDags att spela shotgun!");
+            Console.Write("[ FORTSÄTT ]");
+            Console.ReadKey();
             string playerName = Player.PlayerName().ToUpper();
             string cpuName = Cpu.CpuName();
             Player player = new Player(playerName);
             Player cpu = new Player(cpuName);
-            Console.WriteLine($"\n{player.Name}, ammo: {player.Ammo}");
-            Console.WriteLine($"{cpu.Name}, ammo: {cpu.Ammo}");
-            Console.WriteLine("\n[ FORTSÄTT ]\n");
-            Console.ReadKey();
-            Graphics.MoveMenu(player.Ammo);
-            Console.ReadKey();
+            while (power)
+            {
+                Console.Clear();
+                Graphics.Header();
+                Console.WriteLine($"\n{player.Name}, ammo: {player.Ammo}");
+                Console.WriteLine($"{cpu.Name}, ammo: {cpu.Ammo}\n");
+                int moveChoice = Graphics.MoveMenu(player.Ammo);
+                Console.WriteLine($"Du har valt alternativ {moveChoice}");
+                Console.ReadKey();
+                break;
+            }
 
         }
 
