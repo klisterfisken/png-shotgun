aa4c8a5d9ca2dbf9545af8e5c874e3ddd426ddcb
Hanna <206832485+klisterfisken@users.noreply.github.com>
2025-10-05 09:41:32 +0200

    Tweak some notes to higher octave

--- Music.cs
+++ Music.cs
@@ -1,21 +1,25 @@
 using System;
 using System.Threading;
 
+// Used and modified code from source:
+// https://learn.microsoft.com/en-us/dotnet/api/system.console.beep
+
 class Music
 {
     public static void Play()
     {
-        // Declare the first few notes of the song, "The good, the bad and the ugly".
+        // Declare the first few notes of the song
+        // "The good, the bad and the ugly".
         Note[] Western =
             {
         new Note(Tone.A, Duration.EIGHTH),
-        new Note(Tone.D, Duration.EIGHTH),
+        new Note(Tone.HIGHD, Duration.EIGHTH),
         new Note(Tone.A, Duration.EIGHTH),
-        new Note(Tone.D, Duration.EIGHTH),
+        new Note(Tone.HIGHD, Duration.EIGHTH),
         new Note(Tone.A, Duration.HALF),
-        new Note(Tone.REST, Duration.HALF),
-        new Note(Tone.F, Duration.HALF),
-        new Note(Tone.G, Duration.HALF),
+        new Note(Tone.REST, Duration.EIGHTH),
+        new Note(Tone.F, Duration.QUARTER),
+        new Note(Tone.G, Duration.QUARTER),
         new Note(Tone.D, Duration.HALF),
         };
         // Play the song
@@ -39,7 +43,8 @@ class Music
     protected enum Tone
     {
         REST = 0,
-        A = 220,
+        A = 440,
+        HIGHD = 587,
         D = 294,
         F = 349,
         G = 392,
@@ -74,9 +79,3 @@ class Music
         public Duration NoteDuration { get { return durVal; } }
     }
 }
-/*
-This example produces the following results:
-
-This example plays the first few notes of "Western Had A Little Lamb"
-through the console speaker.
-*/
