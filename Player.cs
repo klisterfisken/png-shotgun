c12f341661c73cb3936b0abc536a8286188be116
Hanna <206832485+klisterfisken@users.noreply.github.com>
2025-10-07 14:04:21 +0200

    Add MoveResolution method

--- Player.cs
+++ Player.cs
@@ -22,5 +22,27 @@ namespace Shotgun
             return playerName;
         }
 
+        public static string MoveResolution(int playerAmmo, int cpuAmmo, string playerMove, string cpuMove)
+        {
+            string message = "";
+            if (playerMove == "SKJUTA")
+            {
+                playerAmmo--;
+                if (cpuMove == "LADDA") message = "Du har vunnit!";
+            }
+            else if (playerMove == "LADDA")
+            {
+                playerAmmo++;
+                if (cpuMove == "SKJUTA") message = "Du förlorade!";
+            }
+            else if (playerMove == "SHOTGUN")
+            {
+                playerAmmo = playerAmmo - 3;
+                if (cpuMove != "SHOTGUN") message = "Du har vunnit!";
+            }
+            else if (cpuMove == "SHOTGUN") message = "Du förlorade!";
+            return message;
+        }
+
     }
 }
